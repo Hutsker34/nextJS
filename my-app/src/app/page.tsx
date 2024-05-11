eca6d1680873b59a6623ece07fe68f938e98f6aa
viacheslav <77201329+viacheslav34@users.noreply.github.com>
2024-05-11 18:28:02 +0300

    add router link

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -40,8 +41,8 @@ export default function Home() {
       </div>
 
       <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+        <Link 
+          href='/page1'
           className={styles.card}
           target="_blank"
           rel="noopener noreferrer"
@@ -50,10 +51,10 @@ export default function Home() {
             Docs <span>-&gt;</span>
           </h2>
           <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+        </Link>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+        <Link
+          href="/page2/page2"
           className={styles.card}
           target="_blank"
           rel="noopener noreferrer"
@@ -62,7 +63,7 @@ export default function Home() {
             Learn <span>-&gt;</span>
           </h2>
           <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+        </Link>
 
         <a
           href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
---
+++ my-app/src/pages/page2/page2.tsx
@@ -0,0 +1,9 @@
+import styles from './page2.module.css'
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.text}>TEST page 2</div>
+    </main>
+  );
+}
